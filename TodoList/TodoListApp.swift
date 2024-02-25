424f234944e456e38f8bcc42dea37fa70361e9d6
Mert Durkaya <mertdurkaya@gmail.com>
2024-02-25 14:53:24 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Mert Durkaya on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
