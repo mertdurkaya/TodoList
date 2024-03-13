5060f6242a66fdd8d7a0884189e041785994a8d6
Mert Durkaya <mertdurkaya@gmail.com>
2024-03-13 21:35:26 +0300

    Readme added, button fix

---
+++ README.md
@@ -0,0 +1,46 @@
+# TodoList
+
+TodoList is a simple task management application built using SwiftUI.
+
+## Running the Project Locally
+
+To run this project locally, follow these steps:
+
+    1. Clone the repository:
+    
+    ```bash
+    git clone https://github.com/mertdurkaya/TodoList.git
+    ```
+    
+    2. Open the project in Xcode.
+
+    3. Build and run the project using the built-in simulator or connect your iOS device and run it.
+
+## Technologies Used
+
+- SwiftUI
+- MVVM
+- UserDefaults
+
+## Additional Information
+
+TodoList is a straightforward task management app designed to help users keep track of their daily tasks. It allows users to create, update, and delete tasks, as well as mark them as completed.
+
+Key features of TodoList include:
+
+- **Simple Interface**: The app features a clean and intuitive user interface, making it easy for users to manage their tasks.
+- **UserDefaults Integration**: UserDefaults is used to persistently store tasks locally on the device, ensuring that tasks are retained even after the app is closed.
+- **Task Management**: Users can add new tasks, edit existing tasks, mark tasks as completed, and delete tasks as needed.
+
+## Contributing
+
+Contributions are welcome! If you find any bugs or have suggestions for improvement, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgments
+
+Thanks to Nick Sarno - https://www.swiftful-thinking.com/
+
--- TodoList/Views/NoItemsView.swift
+++ TodoList/Views/NoItemsView.swift
@@ -11,6 +11,7 @@ struct NoItemsView: View {
     
     @State var animate: Bool = false
     let secondaryAccentColor = Color("SecondaryAccentColor")
+    
     var body: some View {
         ScrollView {
             VStack(spacing: 16) {
@@ -21,24 +22,27 @@ struct NoItemsView: View {
                     .font(.headline)
                     .foregroundColor(.gray)
                     .padding(.bottom, 16)
-                NavigationLink("Add Item 🥳", destination: AddView())
-                    .foregroundStyle(.white)
-                    .font(.title3)
-                    .bold()
-                    .frame(height: 48)
-                    .frame(maxWidth: .infinity)
-                    .background(animate ? secondaryAccentColor : Color.accentColor)
-                    .clipShape(
-                        RoundedRectangle(cornerRadius: 10, style: .continuous)
-                    )
-                    .padding(.horizontal, animate ? 36 : 42)
-                    .shadow(color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
-                            radius: animate ? 32 : 8,
-                            x: 0,
-                            y: animate ? 48 : 32)
-                    .scaleEffect(animate ? 1.05 : 1.0)
-                    .offset(y: animate ? 0 : 3)
-                
+                NavigationLink {
+                    AddView()
+                } label: {
+                    Text("Add an item 🥳")
+                        .foregroundStyle(.white)
+                        .font(.title3)
+                        .bold()
+                        .frame(height: 48)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? secondaryAccentColor : Color.accentColor)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        )
+                        .padding(.horizontal, animate ? 36 : 42)
+                        .shadow(color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                                radius: animate ? 32 : 8,
+                                x: 0,
+                                y: animate ? 48 : 32)
+                        .scaleEffect(animate ? 1.05 : 1.0)
+                        .offset(y: animate ? 0 : 3)
+                }
             }
             .frame(maxWidth: 400)
             .multilineTextAlignment(.center)
@@ -54,8 +58,8 @@ struct NoItemsView: View {
             withAnimation(
                 .easeInOut(duration: 1.5)
                 .repeatForever(autoreverses: true)) {
-                animate.toggle()
-            }
+                    animate.toggle()
+                }
         }
     }
 }
